73d3de3da685526db7daceeb5290befdec6ac6e2
Dave Brunetti <brunetti.dave@gmail.com>
2016-07-09 21:15:26 -0400

    Added lots of JS interactivity

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 var totalTotal = 0;
 var subtotal = 0;
+var counter = 0;
+var shoppingList = [];
 
+function addItemToList() {
+	var item = document.getElementById("add-name").value;
+	document.getElementById("on-list").innerHTML += "<li class='grocery-item' id='" + counter + "'>" + item + "</li>";
+	document.getElementById(counter).getAttribute("onclick");
+	document.getElementById(counter).setAttribute("onclick", "clickToPopulate(" + counter + ")");
+	shoppingList.push(item = {
+		name: item,
+		index: counter
+	});
+	counter++;
+	document.getElementById("add-name").value = "";
+}
 
-function addItem() {
+function clickToPopulate(thisIndex) {
+	document.getElementById('item-name').value = shoppingList[thisIndex].name;
+};
+
+function addItemToCart() {
 	var item = document.getElementById("item-name").value;
 	var price = document.getElementById("item-price").value;
 	var quant = document.getElementById("item-quantity").value;
 	var itemTotal = Number(price * quant);
-	document.getElementById('list').innerHTML += "<li>" + item + "<span class='not-important'> x " + quant + "</span>: $ " + itemTotal.toFixed(2) + "</li>";
+	document.getElementById('cart-list').innerHTML += "<li>" + item + "<span class='not-important'> x" + quant + "</span>: $ " + itemTotal.toFixed(2) + "</li>";
 	subtotal += itemTotal;
 	var tax = subtotal * 0.06;
 	totalTotal = subtotal + tax;
+	checkList();
 	document.getElementById('subtotal').innerHTML = subtotal.toFixed(2);
 	document.getElementById('tax').innerHTML = tax.toFixed(2);
 	document.getElementById('total').innerHTML = totalTotal.toFixed(2);
@@ -18,3 +37,15 @@ function addItem() {
 	document.getElementById('item-price').value = "";
 	document.getElementById('item-quantity').value = 1;
 };
+
+function checkList() {
+	for (var i = 0; i < shoppingList.length; i++) {
+		if (shoppingList[i].name == document.getElementById('item-name').value) {
+			// cross out item on list
+			document.getElementById(i).style.setProperty("text-decoration", "line-through");
+		};
+	};
+};
+
+
+
